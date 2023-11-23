e433cb9d728e00303037a4736893d1b09ce6b6aa
Kelvin Mock <kelvin.mock@qualicom.com>
2023-11-23 16:16:16 -0500

    Redesigned the project as a Next JS project
    project and repo setup

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+          <title>Kelvin's Site</title>
+      </head>
+
+      <body>
+          <h1>Welcome to Kelvin Mock's Website!</h1>
+          <p>Feel free to have a look:)</p>
+          <p>Wow&nbsp&nbspWowww!!!</p>
+      </body>
+    </div>
+  )
+}
