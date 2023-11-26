ae4116377a08f55921dc44fd3674daeee7d5ab23
Kelvin Mock <kelvin.mock@gmail.com>
2023-11-26 00:56:31 -0500

    Fixed Hydration Issue on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,20 @@
 import Image from 'next/image'
 import styles from './styles/page.module.css'
-import { AppBar, Container } from '@mui/material'
 import NavBar from './component/NavBar'
+import React from 'react'
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <head>
-              <title>{"Kelvin's Site"}</title>
-        </head>
-
-        <body>
-          <NavBar title="Welcome to Kelvin Mock's Website!" />
-          <p>
-            <center>
-              {"Feel free to have a look :)"}
-            </center>
-          </p>
-          <p>
-            <center>
-              {"Wow Wowww!!!"}
-            </center>
-          </p>
-        </body>
-      </div>
-    </>
+    <div>
+      <title>{"Kelvin's Site"}</title>
+      
+      <NavBar title="Welcome to Kelvin Mock's Website!" />
+      <p>
+        {"Feel free to have a look :)"}
+      </p>
+      <p>
+        {"Wow Wowww!!!"}
+      </p>      
+    </div>
   )
 }
